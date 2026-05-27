59e3b0974c72d62406efa3d0ae63a7e433d8e1e0
michh18 <michellechan0518@gmail.com>
2026-05-27 12:34:06 +0100

    feat: add default values to Activity parameters

--- BoredButBrokeFE/Components/Models/Activity.cs
+++ BoredButBrokeFE/Components/Models/Activity.cs
@@ -4,17 +4,17 @@ namespace BoredButBrokeFE.Components.Models
 {
     public class Activity
     {
-        public string ActivityId { get; set; }
-        public string ActivityName { get; set; }
-        public string Address { get; set; }
-        public string MainCategory { get; set; }
-        public string SubCategory { get; set; }
-        public Price Price { get; set; }
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-        public Hours Hours { get; set; }
-        public double Rating { get; set; }
-        public string LocationUrl { get; set; }
-        public string PhotoUrl { get; set; }
+        public string ActivityId { get; set; } = "";
+        public string ActivityName { get; set; } = "";
+        public string Address { get; set; } = "";
+        public string MainCategory { get; set; } = "";
+        public string SubCategory { get; set; } = "";
+        public Price Price { get; set; } = Price.Cheap;
+        public double Latitude { get; set; } = 0.00;
+        public double Longitude { get; set; } = 0.00;
+        public Hours Hours { get; set; } = default; 
+        public double Rating { get; set; } = 0.00;
+        public string LocationUrl { get; set; } = "";
+        public string PhotoUrl { get; set; } = "";
     }
 }
