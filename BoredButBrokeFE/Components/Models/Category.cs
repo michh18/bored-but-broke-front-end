a403e6135a94ea639134907e76ee7f5d3b101cc7
michh18 <michellechan0518@gmail.com>
2026-05-27 17:01:37 +0100

    feat: update data Models to correlate with new Yelp API

---
+++ BoredButBrokeFE/Components/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace BoredButBrokeFE.Components.Models
+{
+    public class Category
+    {
+        public string Alias { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+    }
+}
---
+++ BoredButBrokeFE/Components/Models/OpeningHours.cs
@@ -0,0 +1,9 @@
+﻿namespace BoredButBrokeFE.Components.Models
+{
+    public class OpeningHours
+    {
+        public List<Hour> Hours { get; set; } = new List<Hour>();
+        public string HoursType { get; set; } = string.Empty;
+        public bool IsOpenNow { get; set; } = false;
+    }
+}
