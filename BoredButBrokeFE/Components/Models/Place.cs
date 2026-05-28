e308d991a7ef8a4a2532334cc1d484e2b712a89b
michh18 <michellechan0518@gmail.com>
2026-05-28 14:56:58 +0100

    refactor: update type of OpeningHours variable to be List<OpeningHours>

--- BoredButBrokeFE/Components/Models/Place.cs
+++ BoredButBrokeFE/Components/Models/Place.cs
@@ -11,7 +11,7 @@ namespace BoredButBrokeFE.Components.Models
         public List<Category> Categories { get; set; } = new List<Category>();
         public Price Price { get; set; } = Price.Cheap;
         public Coordinates Coordinates { get; set; } = new Coordinates();
-        public OpeningHours OpeningHours { get; set; } = new OpeningHours(); 
+        public List<OpeningHours> OpeningHours { get; set; } = new List<OpeningHours>(); 
         public double Rating { get; set; } = 1;
         public string PlaceUrl { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
