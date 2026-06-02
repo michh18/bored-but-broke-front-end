cfb8c54fb28e74af35e2a4a72ee9654dec9e5f78
michh18 <michellechan0518@gmail.com>
2026-06-02 17:08:38 +0100

    fix: add JsonPropertyName to variables to fix unmatched casing

--- BoredButBrokeFE/Components/Models/Category.cs
+++ BoredButBrokeFE/Components/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace BoredButBrokeFE.Components.Models
+﻿using System.Text.Json.Serialization;
+
+namespace BoredButBrokeFE.Components.Models
 {
     public class Category
     {
+        [JsonPropertyName("alias")]
         public string Alias { get; set; } = string.Empty;
+        [JsonPropertyName("title")]
         public string Title { get; set; } = string.Empty;
     }
 }
