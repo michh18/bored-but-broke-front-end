9b36b8fb97695f675a9ed51615102e45485a05e9
Chris <211069418+chris-mech@users.noreply.github.com>
2026-06-03 12:03:08 +0100

    feat: add auth DTOs

---
+++ BoredButBrokeFE/Components/Models/Auth/LoginUserRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoredButBrokeFE.Components.Models.Auth
+{
+    public class LoginUserRequest
+    {
+        [Required(ErrorMessage = "Email must not be empty")]
+        public required string Email { get; set; }
+        [Required(ErrorMessage = "Password must not be empty")]
+        public required string Password { get; set; }
+    }
+}
---
+++ BoredButBrokeFE/Components/Models/Auth/UserInfoResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace BoredButBrokeFE.Components.Models.Auth
+{
+    public class UserInfoResponse
+    {
+        public string? Email { get; set; }
+        public string? FirstName { get; set; }
+    }
+}
