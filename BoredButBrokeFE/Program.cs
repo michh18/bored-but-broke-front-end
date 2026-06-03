4d01baacfec2fc39fecbe9c35db860415e9cbd34
Chris <211069418+chris-mech@users.noreply.github.com>
2026-06-03 21:58:34 +0100

    feat: add logout button and refactor interactive mode

--- BoredButBrokeFE/Program.cs
+++ BoredButBrokeFE/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthentication();
 builder.Services.AddAuthorizationCore();
 
