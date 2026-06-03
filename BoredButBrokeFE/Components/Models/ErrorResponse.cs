d84082b6a91ada3a1efba8a0ec683cbd37b653e8
Chris <211069418+chris-mech@users.noreply.github.com>
2026-06-03 14:50:49 +0100

    feat: add basic register functionality

---
+++ BoredButBrokeFE/Components/Models/ErrorResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BoredButBrokeFE.Components.Models
+{
+    public record ErrorResponse
+    {
+        [JsonPropertyName("title")]
+        public string? Title { get; set; }
+
+        [JsonPropertyName("detail")]
+        public string? Detail { get; set; }
+    }
+}
