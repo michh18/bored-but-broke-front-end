1305e2cc43e245d6237b54153660d5463c4f4f70
yumna <yumnaeltaba@gmail.com>
2026-06-05 11:13:05 +0100

    made the map readable from blazor to the leaflet

--- BoredButBrokeFE/wwwroot/js/mapInterop.js
+++ BoredButBrokeFE/wwwroot/js/mapInterop.js
@@ -1,1 +1,35 @@
-﻿
+﻿let mapInstance;
+
+window.mapInterop = {
+
+    initializeMap: function (elementId, lat, lng, zoomLevel) {
+
+        if (mapInstance) {
+            mapInstance.remove();
+        }
+
+        mapInstance = L.map(elementId)
+            .setView([lat, lng], zoomLevel);
+
+        L.tileLayer(
+            'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            {
+                maxZoom: 19,
+                attribution: '© OpenStreetMap contributors'
+            })
+            .addTo(mapInstance);
+
+        L.marker([lat, lng])
+            .addTo(mapInstance);
+    },
+
+    panToLocation: function (lat, lng) {
+
+        if (mapInstance) {
+
+            mapInstance.panTo(
+                new L.LatLng(lat, lng)
+            );
+        }
+    }
+};
