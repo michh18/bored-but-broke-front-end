43b3c9d5c2c5531b3c2fea0fffe1f2a7ab8d3a7e
yumna <yumnaeltaba@gmail.com>
2026-06-05 13:29:38 +0100

    tried fixing thesquar bug (unsuccessful)

--- BoredButBrokeFE/wwwroot/js/mapInterop.js
+++ BoredButBrokeFE/wwwroot/js/mapInterop.js
@@ -11,16 +11,27 @@ window.mapInterop = {
         mapInstance = L.map(elementId)
             .setView([lat, lng], zoomLevel);
 
+        setTimeout(() => {
+            mapInstance.invalidateSize();
+        }, 300);
+
+        setTimeout(() => {
+            mapInstance.invalidateSize();
+        }, 1000);
+
         L.tileLayer(
             'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
             {
                 maxZoom: 19,
                 attribution: '© OpenStreetMap contributors'
-            })
-            .addTo(mapInstance);
+            }).addTo(mapInstance);
 
         L.marker([lat, lng])
             .addTo(mapInstance);
+
+        requestAnimationFrame(() => {
+            mapInstance.invalidateSize();
+        });
     },
 
     panToLocation: function (lat, lng) {
